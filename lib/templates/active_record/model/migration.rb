56f4b063a8e64ee6631cd63290e4b8ed3a0aaf21
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-06-15 10:14:01 -0300

    Se agregan los templates modificados del scaffold. Se agrega 'jquery-min' a las librerías default de la aplicación

---
+++ lib/templates/active_record/model/migration.rb
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+class <%= migration_class_name %> < ActiveRecord::Migration
+  def self.up
+    create_table :<%= table_name %> do |t|
+<% for attribute in attributes -%>
+      t.<%= attribute.type %> :<%= attribute.name %>
+<% end -%>
+<% if options[:timestamps] %>
+      t.timestamps
+<% end -%>
+    end
+  end
+
+  def self.down
+    drop_table :<%= table_name %>
+  end
+end
